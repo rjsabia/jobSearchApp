eea1f9a6d471c1665d10d37810c08ed4ff434e12
Russell Sabia <rjsabia@gmail.com>
2017-02-03 11:17:22 -0800

    now using github API, function but needs layout and more js output work

--- js/app.js
+++ js/app.js
@@ -1,38 +1,3 @@
-// takes a string of semi-colon separated tags to be searched
-// for on StackOverflow
-// var getUnanswered = function(tags) {
-  
-//  // the parameters we need to pass in our request to StackOverflow's API
-//  var request = { 
-//    tagged: tags,
-//    site: 'stackoverflow',
-//    order: 'desc',
-//    sort: 'creation'
-//  };
-  
-//  $.ajax({
-//    url: "https://api.stackexchange.com/2.2/questions/unanswered",
-//    data: request,
-//    dataType: "jsonp",//use jsonp to avoid cross origin issues
-//    type: "GET"
-//  })
-//  .done(function(result){ //this waits for the ajax to return with a succesful promise object
-    
-//    var searchResults = showSearchResults(request.tagged, result.items.length);
-
-//    console.log(result);
-
-//    $('.search-results').html(searchResults);
-//    //$.each is a higher order function. It takes an array and a function as an argument.
-//    //The function is executed once for each item in the array.
-//    $.each(result.items, function(i, item) {
-//      var question = showQuestion(item);
-//      $('.results').append(question);
-//    });
-//  })
-
-//**********************************************************************
-
 $(document).ready( function() {
   // This grabs the submit for un-answered questions
   $('#input-form').submit( function(e){
@@ -42,8 +7,9 @@ $(document).ready( function() {
     // $('#search-data').html('');
     // get the value of the tags the user submitted
     var jobSearch = $('#search-data').val();
+    var location = $('#search-location').val();
     //function call below
-    
+    getJobs(jobSearch,location);
     // clears out input bar after search is run
     $('#search-data').val('');
 
@@ -52,5 +18,39 @@ $(document).ready( function() {
   });
   
 });
+
+var getJobs = function(job_title,loc) {
+ 
+  var request = { 
+    description: job_title,
+    location: loc,
+    full_time: true
+  };
+  
+  $.ajax({
+    url: "http://jobs.github.com/positions.json?",
+    data: request,
+    dataType: "jsonp",//use jsonp to avoid cross origin issues
+    type: "GET"
+  })
+  .done(function(result){ //this waits for the ajax to return with a succesful promise object
+    
+   // var searchResults = showSearchResults(request.tagged, result.items.length);
+
+    console.log(result);
+
+    $.each(result, function(key,value){
+      console.log(value);
+      $('#output-container').html(value.company);
+    })
+
+    
+
+   // $('.search-results').html(searchResults);
+   
+ });
+}
+
+
 // **********END OF PROGRAM*********************************************
 // *********************************************************************
